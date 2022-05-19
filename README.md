dc910071fde32f72283a4a49dcab7bb9adcfe6ca
Kirill Morozov <moki-codes@yandex-team.ru>
2022-05-19 12:54:42 +0300

    notes

---
+++ README.md
@@ -0,0 +1,16 @@
+# sentenizer
+
+## deps
+
+* python3 
+
+## notes
+  notes on the natasha/razdel sentenizer implementation
+
+  `notes.txt`
+
+## instructions
+
+```
+  python3 main.py
+```
---
+++ lib/split.py
@@ -0,0 +1,17 @@
+
+
+from lib.record import Record
+
+
+class Split(Record):
+    __attributes__ = ['left', 'delimiter', 'right', 'buffer']
+
+    def __init__(self, left, delimiter, right, buffer=None):
+        self.left = left
+        self.delimiter = delimiter
+        self.right = right
+        self.buffer = buffer
+
+
+class Splitter(Record):
+    pass
