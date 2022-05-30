f942214b89f85479095173990d9716787f3ef423
moki-codes <moki-codes@yandex-team.ru>
2022-05-30 20:14:22 +0300

    feat(utilities): utility functions implementation

---
+++ src/utilities/string.ts
@@ -0,0 +1,14 @@
+import {compose, invoker, juxt, allPass, identity, not, toLower, toUpper} from 'ramda';
+
+import {allEqual} from './list';
+
+const charAt = invoker(1, 'charAt');
+
+const notAlpha = compose(allEqual, juxt([toLower, toUpper]));
+
+const startsWithLower = allPass([
+    compose(compose(not, notAlpha), charAt(0)),
+    compose(allEqual, juxt([identity, toLower]), charAt(0)),
+]);
+
+export {charAt, notAlpha, startsWithLower};
