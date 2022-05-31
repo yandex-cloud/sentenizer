780c8f6987c76012e8d779870d0ab31758ae2395
moki-codes <moki-codes@yandex-team.ru>
2022-05-31 20:01:54 +0300

    feat(rules): base join rules implementation

---
+++ src/rules/index.ts
@@ -0,0 +1,10 @@
+export {
+    spaceBothSides,
+    rightLacksSpacePrefix,
+    rightStartsWithLowercase,
+    rightDelimiterPrefix,
+    rightQuotationGenericPrefix,
+    rightQuotationClosePrefix,
+    rightBracketsClosePrefix,
+    rightOnlySpaces,
+} from './base';
