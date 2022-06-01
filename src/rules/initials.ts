250704789de28f46d26b77ccbea29afd77d427a9
moki-codes <moki-codes@yandex-team.ru>
2022-06-01 16:27:50 +0300

    feat(rules): initials

---
+++ src/rules/initials.ts
@@ -0,0 +1,31 @@
+import {
+    toUpper,
+    identity,
+    juxt,
+    length,
+    equals,
+    allPass,
+    compose,
+    call,
+    always,
+    all,
+    zipWith,
+} from 'ramda';
+
+import {lengthNonZero, allEqual} from '../utilities';
+import {dotSuffix, lstWord} from '../parsers';
+
+// determine if left is part of the initials
+// conditions:
+//     * left delimiter is dot
+//     * left last word is single letter
+//     * left last word is in upper case
+const isLeftDotDelimiter = compose(lengthNonZero, dotSuffix);
+const isLeftSingleLetter = compose(equals(1), length, lstWord);
+const isLeftUpper = compose(allEqual, juxt([toUpper, identity]), lstWord);
+
+const isLeftInitials = allPass([isLeftDotDelimiter, isLeftSingleLetter, isLeftUpper]);
+
+const leftInitials = compose(all(Boolean), zipWith(call, [isLeftInitials, always(true)]));
+
+export {leftInitials};
