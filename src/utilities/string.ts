0bafdf37fdb7f831fa0a5bacc33adbfabc8f167e
moki-codes <moki-codes@yandex-team.ru>
2022-06-03 15:53:57 +0300

    feat(utilitie): hasAlpha
    determine if strings contains alpha symbols

--- src/utilities/string.ts
+++ src/utilities/string.ts
@@ -6,9 +6,11 @@ const charAt = invoker(1, 'charAt');
 
 const notAlpha = compose(allEqual, juxt([toLower, toUpper]));
 
+const hasAlpha = compose(not, notAlpha);
+
 const startsWithLower = allPass([
     compose(compose(not, notAlpha), charAt(0)),
     compose(allEqual, juxt([identity, toLower]), charAt(0)),
 ]);
 
-export {charAt, notAlpha, startsWithLower};
+export {charAt, notAlpha, hasAlpha, startsWithLower};
