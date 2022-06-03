cbc71483eab1988ea152a38bf873514db0f72829
moki-codes <moki-codes@yandex-team.ru>
2022-06-03 15:56:22 +0300

    fix(rules): initials
    single digit shouldnt be treated as part of the initials

--- src/rules/initials.ts
+++ src/rules/initials.ts
@@ -12,7 +12,7 @@ import {
     zipWith,
 } from 'ramda';
 
-import {lengthNonZero, allEqual} from '../utilities';
+import {hasAlpha, lengthNonZero, allEqual} from '../utilities';
 import {dotSuffix, lstWord} from '../parsers';
 
 // determine if left is part of the initials
@@ -20,11 +20,13 @@ import {dotSuffix, lstWord} from '../parsers';
 //     * left delimiter is dot
 //     * left last word is single letter
 //     * left last word is in upper case
+//     * left has alpha characters
 const isLeftDotDelimiter = compose(lengthNonZero, dotSuffix);
 const isLeftSingleLetter = compose(equals(1), length, lstWord);
 const isLeftUpper = compose(allEqual, juxt([toUpper, identity]), lstWord);
+const leftHasAlpha = compose(hasAlpha, lstWord);
 
-const isLeftInitials = allPass([isLeftDotDelimiter, isLeftSingleLetter, isLeftUpper]);
+const isLeftInitials = allPass([isLeftDotDelimiter, isLeftSingleLetter, isLeftUpper, leftHasAlpha]);
 
 const leftInitials = compose(all(Boolean), zipWith(call, [isLeftInitials, always(true)]));
 
